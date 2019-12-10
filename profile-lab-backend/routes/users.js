916c4adfea2c64b2cba95be1bbc7fdb76f6e6c31
Julian Phillips <julian.p@me.com>
2019-12-10 12:02:27 -0500

    feat: removing initial project.

--- profile-lab-backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../Models/User");
-
-router.get("/", (req, res, next) => {
-  res.render("Hello from User route");
-});
-
-module.exports = router;
